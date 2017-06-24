5719337a5ab28d77a795ed1856fde75f8fd36807
pratchapong <pratchapong.bo@gmail.com>
2017-06-24 21:47:09 +0700

    Feed

--- js/controller.js
+++ js/controller.js
@@ -3,7 +3,7 @@ app.controller('FeedController', FeedController)
 app.controller('CaptureController', CaptureController)
 app.controller('ProfileController', ProfileController)
 app.controller('GeolocationController', GeolocationController)
-
+app.run(['$safeApply', function() {}]);
 /** @ngInject */
 function IndexController($scope) {
     var vm = this;
@@ -16,22 +16,47 @@ function IndexController($scope) {
 
 function FeedController($scope, FirebaseService) {
     var vm = this;
-    var refProfile = firebase.database().ref().child('/profile');
+    vm.profileList = [];
 
-    refProfile.orderByKey().on("value", function(snapshot) {
-      
-            vm.profileList = [];
-            snapshot.forEach(function(data) {
-                console.log('data ::==', data.key);
-                var _data = snapshot.child(data.key).val();
-                _data.id = data.key
+// Firebase
+    var refProfile = firebase.database().ref().child('/profile');
+    refProfile.orderByKey().on("value", function (snapshot) {
+        $scope.$safeApply(function() {
+        snapshot.forEach(function (data) {
+            var _id = data.key;
+            console.log('data ::==', _id);
+            var _data = snapshot.child(_id).val();
+            console.log('_data ::==', _data);
+
+            // Galllery List
+            var refGallery = firebase.database().ref().child('/gallery');
+            refGallery.orderByChild("user_id").equalTo(_id).on("value", function (subSnapshot) {
+                 $scope.$safeApply(function() {
+                _data.gallerys = [];
+                subSnapshot.forEach(function (data) {
+                    var _dataSub = subSnapshot.child(data.key).val();
+                    console.log('_dataSub ::==', _dataSub);
+                    _data.gallerys.push(_dataSub);
+                });
                 vm.profileList.push(_data);
+                console.log("vm.profileList :: " ,vm.profileList);
+                });
+            }, function (error) {
+                console.log("Error: " + error.code);
             });
-    }, function(error) {
+
+        });
+        });
+    }, function (error) {
         console.log("Error: " + error.code);
     });
+
+//-----------------------------------------------------------------
+ console.log(vm.profileList);
+//-----------------------------------------------------------------
+
     //Title
-    vm.name = 'Feed';
+    vm.title = 'Feed';
 
 
 }
