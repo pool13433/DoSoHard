535bda11b62a7c8528e6e093ecc4ffe98a3f83d1
tent2613 <tent2631@gmail.com>
2017-06-25 13:43:42 +0700

    login

--- js/controllers/login.js
+++ js/controllers/login.js
@@ -1,3 +1,89 @@
-function LoginController() {
+function LoginController(FirebaseService) {
+    var vm = this;
+       console.log("TEST");
 
+    vm.facebook = function (){
+var provider = new firebase.auth.FacebookAuthProvider();
+provider.addScope('public_profile');
+provider.addScope('email');
+//provider.addScope('user_location');
+
+provider.setCustomParameters({
+  'display': 'popup'
+});
+
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+
+  console.log("User",user);
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  console.log("Error",error);
+  // ...
+});
+
+}
+
+vm.google = function (){
+    var provider = new firebase.auth.GoogleAuthProvider();
+   // provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+   var user = firebase.auth().currentUser;
+
+if (user != null) {
+  user.providerData.forEach(function (profile) {
+    console.log("Sign-in provider: "+profile.providerId);
+    console.log("  Provider-specific UID: "+profile.uid);
+    console.log("  Name: "+profile.displayName);
+    console.log("  Email: "+profile.email);
+    console.log("  Photo URL: "+profile.photoURL);
+  });
+}else{
+    
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+
+  /*user.sendEmailVerification().then(function() {
+  console.log("sendEmailVerification suc");
+}, function(error) {
+  console.log("sendEmailVerification fail",error);
+});*/
+  
+  console.log("User",user);
+  console.log("Username",user.displayName);
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+   console.log("Error",error);
+  // ...
+});
+}
+    vm.signOut = function(){
+        firebase.auth().signOut().then(function() {
+        console.log("sing-out suc");
+    }).catch(function(error) {
+        console.log("sing-out fail",error);
+    });
+    }
+
+    
+}
 }
