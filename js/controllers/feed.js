e0dfa93b45a13deab1c9d76d58d8bcdc39d16860
pratchapong <pratchapong.bo@gmail.com>
2017-06-25 12:58:45 +0700

    Feed

--- js/controllers/feed.js
+++ js/controllers/feed.js
@@ -26,34 +26,63 @@ function FeedController($scope, FirebaseService) {
         $scope.$safeApply(function() {
         snapshot.forEach(function (data) {
             var _id = data.key;
-            console.log('data ::==', _id);
+           // console.log('data ::==', _id);
             var _data = snapshot.child(_id).val();
-            console.log('_data ::==', _data);
-
+           // console.log('_data ::==', _data);
+           var total_rate = 0 ;
             // Galllery List
+
+
+
             var refGallery = firebase.database().ref().child('/gallery');
             refGallery.orderByChild("user_id").equalTo(_id).on("value", function (subSnapshot) {
                  $scope.$safeApply(function() {
                 _data.gallerys = [];
                 subSnapshot.forEach(function (data) {
                     var _dataSub = subSnapshot.child(data.key).val();
-                    console.log('_dataSub ::==', _dataSub);
+                   // console.log('_dataSub ::==', _dataSub);
                     _data.gallerys.push(_dataSub);
+
                 });
                 vm.profileList.push(_data);
                 console.log("vm.profileList :: " ,vm.profileList);
                 });
             }, function (error) {
                 console.log("Error: " + error.code);
-            });
+            });  
+        });
+        
 
-        });
-        });
+
+
+
+     });
     }, function (error) {
         console.log("Error: " + error.code);
     });
 
-//-----------------------------------------------------------------
+   /* vm.galleryList = [];
 
+    var refGalleryRate = firebase.database().ref().child('/gallery');
+    refProfile.orderByKey().on("value", function (snapshot) {
+         $scope.$safeApply(function() {
+            snapshot.forEach(function(data){
+                 var _id = data.key;
+           // console.log('data ::==', _id);
+                var _dataGal = snapshot.child(_id).val();
+               
+                vm.galleryList.push(_dataGal);
+
+            });
+             console.log("OOOOOOOOOOOOOO",vm.galleryList)
+         });
+    }, function (error) {
+        console.log("Error: " + error.code);
+    });*/
+
+//-----------------------------------------------------------------
+console.log("------------------------------------------------------");
+
+//-----------------------------------------------------------------
 
 }
