99295a004fbf19439e54787588a1e1511a58733c
pool13433 <poon_mp@hotmail.com>
2017-06-25 15:38:22 +0700

    Fixed Login

--- js/controllers/login.js
+++ js/controllers/login.js
@@ -1,89 +1,90 @@
-function LoginController(FirebaseService) {
+function LoginController(FirebaseService, AuthenticationService, $location, $scope,$rootScope) {
     var vm = this;
-       console.log("TEST");
+    console.log("TEST");
 
-    vm.facebook = function (){
-var provider = new firebase.auth.FacebookAuthProvider();
-provider.addScope('public_profile');
-provider.addScope('email');
-//provider.addScope('user_location');
+    vm.facebook = function () {
+        var provider = new firebase.auth.FacebookAuthProvider();
+        provider.addScope('public_profile');
+        provider.addScope('email');
+        //provider.addScope('user_location');
 
-provider.setCustomParameters({
-  'display': 'popup'
-});
+        provider.setCustomParameters({
+            'display': 'popup'
+        });
 
-firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
+        firebase.auth().signInWithPopup(provider).then(function (result) {
+            // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
 
-  console.log("User",user);
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  console.log("Error",error);
-  // ...
-});
+            console.log("User", user);
+            $scope.$safeApply(function () {
+                AuthenticationService.SetCredentials(user);
+                $location.path('/feed');
+                console.log(" location.path ::");
+            });
+            // ...
+        }).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            console.log("Error", error);
+            // ...
+        });
 
-}
-
-vm.google = function (){
-    var provider = new firebase.auth.GoogleAuthProvider();
-   // provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-   var user = firebase.auth().currentUser;
-
-if (user != null) {
-  user.providerData.forEach(function (profile) {
-    console.log("Sign-in provider: "+profile.providerId);
-    console.log("  Provider-specific UID: "+profile.uid);
-    console.log("  Name: "+profile.displayName);
-    console.log("  Email: "+profile.email);
-    console.log("  Photo URL: "+profile.photoURL);
-  });
-}else{
-    
-firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Google Access Token. You can use it to access the Google API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-
-  /*user.sendEmailVerification().then(function() {
-  console.log("sendEmailVerification suc");
-}, function(error) {
-  console.log("sendEmailVerification fail",error);
-});*/
-  
-  console.log("User",user);
-  console.log("Username",user.displayName);
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-   console.log("Error",error);
-  // ...
-});
-}
-    vm.signOut = function(){
-        firebase.auth().signOut().then(function() {
-        console.log("sing-out suc");
-    }).catch(function(error) {
-        console.log("sing-out fail",error);
-    });
     }
 
-    
-}
+    vm.google = function () {
+        var provider = new firebase.auth.GoogleAuthProvider();
+        // provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+        var user = firebase.auth().currentUser;
+
+        if (user != null) {
+            user.providerData.forEach(function (profile) {
+                console.log("Sign-in provider: " + profile.providerId);
+                console.log("  Provider-specific UID: " + profile.uid);
+                console.log("  Name: " + profile.displayName);
+                console.log("  Email: " + profile.email);
+                console.log("  Photo URL: " + profile.photoURL);
+            });
+        } else {
+
+            firebase.auth().signInWithPopup(provider).then(function (result) {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                var token = result.credential.accessToken;
+                // The signed-in user info.
+                var user = result.user;
+
+                /*user.sendEmailVerification().then(function() {
+                console.log("sendEmailVerification suc");
+              }, function(error) {
+                console.log("sendEmailVerification fail",error);
+              });*/
+
+                console.log("User", user);
+                console.log("Username", user.displayName);
+                $scope.$safeApply(function () {
+                    AuthenticationService.SetCredentials(user);
+                    $location.path('/feed');
+                    console.log(" location.path ::");
+                });
+                // ...
+            }).catch(function (error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // The email of the user's account used.
+                var email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                var credential = error.credential;
+                console.log("Error", error);
+                // ...
+            });
+        }
+    }
 }
--- js/factory.js
+++ js/factory.js
@@ -3,19 +3,38 @@ app.factory('Utility', Utility);
 app.factory('GeolocationFactory', GeolocationFactory);
 app.factory('$safeApply', ['$rootScope', safeApply]);
 
-app.filter('split', function() {
-    return function(input, splitChar, splitIndex) {
+app.filter('split', function () {
+    return function (input, splitChar, splitIndex) {
         // do some bounds checking here to ensure it has that index
         return input.split(splitChar)[splitIndex];
     }
 });
+
+app.factory('AuthenticationService', ['$cookieStore', '$rootScope', AuthenticationService]);
+
+
+function AuthenticationService($cookieStore, $rootScope) {
+    var Authentication = {};
+
+    Authentication.SetCredentials = function (firebaseUser) {
+        $rootScope.firebaseUser = firebaseUser
+        $cookieStore.put('globals', $rootScope.firebaseUser);
+    }
+
+    Authentication.ClearCredentials = function () {
+        $rootScope.firebaseUser = null;
+        $cookieStore.remove('firebaseUser');
+    };
+
+    return Authentication;
+}
 
 function GeolocationFactory() {
     var Geolocation = {}
 
     Geolocation.init = function () {
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function(loc){
+            navigator.geolocation.getCurrentPosition(function (loc) {
                 Geolocation.coords = loc.coords;
             });
         }
