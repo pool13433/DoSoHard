52cb9ed0a6fd914b675721a74166af489f4b3f26
pool13433 <poon_mp@hotmail.com>
2017-06-25 09:40:31 +0700

    - Fixed mainifest.json , service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-importScripts('workbox-sw.prod.v1.0.1.js');
+//importScripts('workbox-sw.prod.v1.0.1.js');
+importScripts('./node_modules/workbox-sw/build/importScripts/workbox-sw.dev.v1.0.1.js');
 
 /**
  * DO NOT EDIT THE FILE MANIFEST ENTRY
