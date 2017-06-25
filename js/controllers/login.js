f88dea71724990f5f3a4d6f3b84834a0b194189c
tent2613 <tent2631@gmail.com>
2017-06-25 18:57:08 +0700

    successsss login

--- js/controllers/login.js
+++ js/controllers/login.js
@@ -2,43 +2,6 @@ function LoginController(FirebaseService, AuthenticationService, $location, $sco
     var vm = this;
     console.log("TEST");
 
-    vm.facebook = function () {
-        var provider = new firebase.auth.FacebookAuthProvider();
-        provider.addScope('public_profile');
-        provider.addScope('email');
-        //provider.addScope('user_location');
-
-        provider.setCustomParameters({
-            'display': 'popup'
-        });
-
-        firebase.auth().signInWithPopup(provider).then(function (result) {
-            // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-            var token = result.credential.accessToken;
-            // The signed-in user info.
-            var user = result.user;
-
-            console.log("User", user);
-            $scope.$safeApply(function () {
-                AuthenticationService.SetCredentials(user);
-                $location.path('/feed');
-                console.log(" location.path ::");
-            });
-            // ...
-        }).catch(function (error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-            console.log("Error", error);
-            // ...
-        });
-
-    }
-
     vm.google = function () {
         var provider = new firebase.auth.GoogleAuthProvider();
         // provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
@@ -60,11 +23,11 @@ function LoginController(FirebaseService, AuthenticationService, $location, $sco
                 // The signed-in user info.
                 var user = result.user;
 
-                /*user.sendEmailVerification().then(function() {
+                user.sendEmailVerification().then(function() {
                 console.log("sendEmailVerification suc");
               }, function(error) {
                 console.log("sendEmailVerification fail",error);
-              });*/
+              });
 
                 console.log("User", user);
                 console.log("Username", user.displayName);
