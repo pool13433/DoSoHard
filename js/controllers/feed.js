4632fab4d055874067b021fc9147223836fa1d18
pratchapong <pratchapong.bo@gmail.com>
2017-06-25 15:41:53 +0700

    Feed

--- js/controllers/feed.js
+++ js/controllers/feed.js
@@ -20,12 +20,16 @@ function FeedController($scope, FirebaseService) {
                 refGallery.orderByChild("user_id").equalTo(_id).on("value", function (subSnapshot) {
                     $scope.$safeApply(function () {
                         _data.gallerys = [];
+                       // _data.tatal_rate= [];
+                        var totolRate = 0 ;
                         subSnapshot.forEach(function (data) {
                             var _dataSub = subSnapshot.child(data.key).val();
-                            _data.gallerys.push(_dataSub);
+                            
+
+
                             //console.log(JSON.stringify(_dataSub));
                             //console.log('_dataSub ::==', _dataSub);                            
-                            /*var gallerys_rate = _dataSub.gallery_rate;
+                            var gallerys_rate = _dataSub.gallery_rate;
 
                             var sumGallery = _(gallerys_rate).groupBy('user_id').map(function (value, key) {
                                 //console.log('key ::==',key);
@@ -36,9 +40,19 @@ function FeedController($scope, FirebaseService) {
                                 }
                             }).value();
                             console.log('_data ::==', _data.name);
-                            console.log('sumGallery ::==', sumGallery);*/
-                            //_data.gallerys.push(_dataSub);
+                            console.log('sumGallery ::==', sumGallery);
+
+                            console.log('---------------------');
+                            var total = _.sumBy(sumGallery,function(total){
+                                return total.count;
+                            });
+                            console.log('total ::==', total);
+                            totolRate += total;
+                            _data.gallerys.push(_dataSub);
+
+
                         });
+                        _data.total_rate = totolRate; 
                         vm.profileList.push(_data);
                         console.log("vm.profileList :: " ,vm.profileList);
                     });
