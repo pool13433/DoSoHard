64cfd4c1dd8c103d4d36212fd30280b87ad6a8d9
pool13433 <poon_mp@hotmail.com>
2017-06-25 12:50:52 +0700

    add Lodash js

--- js/controllers/feed.js
+++ js/controllers/feed.js
@@ -1,7 +1,7 @@
 function FeedController($scope, FirebaseService) {
     var vm = this;
     vm.profileList = [];
-       
+
     /*var refProfile = firebase.database().ref().child('/profile');
 
     refProfile.orderByKey().on("value", function (snapshot) {
@@ -20,40 +20,53 @@ function FeedController($scope, FirebaseService) {
     vm.name = 'Feed';
 
     //------------------------------------------------------------------
-// Firebase
+    // Firebase
     var refProfile = firebase.database().ref().child('/profile');
     refProfile.orderByKey().on("value", function (snapshot) {
-        $scope.$safeApply(function() {
-        snapshot.forEach(function (data) {
-            var _id = data.key;
-            console.log('data ::==', _id);
-            var _data = snapshot.child(_id).val();
-            console.log('_data ::==', _data);
+        $scope.$safeApply(function () {
+            snapshot.forEach(function (data) {
+                var _id = data.key;
+                console.log('data ::==', _id);
+                var _data = snapshot.child(_id).val();
+                console.log('_data ::==', _data.name);
 
-            // Galllery List
-            var refGallery = firebase.database().ref().child('/gallery');
-            refGallery.orderByChild("user_id").equalTo(_id).on("value", function (subSnapshot) {
-                 $scope.$safeApply(function() {
-                _data.gallerys = [];
-                subSnapshot.forEach(function (data) {
-                    var _dataSub = subSnapshot.child(data.key).val();
-                    console.log('_dataSub ::==', _dataSub);
-                    _data.gallerys.push(_dataSub);
+                // Galllery List
+                var refGallery = firebase.database().ref().child('/gallery');
+                refGallery.orderByChild("user_id").equalTo(_id).on("value", function (subSnapshot) {
+                    $scope.$safeApply(function () {
+                        _data.gallerys = [];
+                        subSnapshot.forEach(function (data) {
+                            var _dataSub = subSnapshot.child(data.key).val();
+                            //console.log(JSON.stringify(_dataSub));
+                            //console.log('_dataSub ::==', _dataSub);                            
+                            var gallerys_rate = _dataSub.gallery_rate;
+
+                            var sumGallery = _(gallerys_rate).groupBy('user_id').map(function (value, key) {
+                                //console.log('key ::==',key);
+                                //console.log('value ::==',value);
+                                return {
+                                    name: key,
+                                    count: _.countBy(value, 'user_id')[key]
+                                }
+                            }).value();
+                            console.log('_data ::==', _data.name);
+                            console.log('sumGallery ::==', sumGallery);
+                            //_data.gallerys.push(_dataSub);
+                        });
+                        vm.profileList.push(_data);
+                        //console.log("vm.profileList :: " ,vm.profileList);
+                    });
+                }, function (error) {
+                    console.log("Error: " + error.code);
                 });
-                vm.profileList.push(_data);
-                console.log("vm.profileList :: " ,vm.profileList);
-                });
-            }, function (error) {
-                console.log("Error: " + error.code);
+
             });
-
-        });
         });
     }, function (error) {
         console.log("Error: " + error.code);
     });
 
-//-----------------------------------------------------------------
+    //-----------------------------------------------------------------
 
 
 }
