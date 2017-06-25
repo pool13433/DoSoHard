df42e2f7c2be9fa4a662d5d6583396ffdcc2da99
pratchapong <pratchapong.bo@gmail.com>
2017-06-25 19:33:19 +0700

    Feed

--- js/controllers/feed.js
+++ js/controllers/feed.js
@@ -1,7 +1,7 @@
-function FeedController($scope, FirebaseService) {
+function FeedController($scope, FirebaseService, $rootScope) {
     var vm = this;
     vm.profileList = [];
-    vm.name = 'Feed';
+    vm.title = 'Feed';
 
     //------------------------------------------------------------------
     // Firebase
@@ -22,6 +22,7 @@ function FeedController($scope, FirebaseService) {
                         _data.gallerys = [];
                        // _data.tatal_rate= [];
                         var totolRate = 0 ;
+
                         subSnapshot.forEach(function (data) {
                             var _dataSub = subSnapshot.child(data.key).val();
                             
@@ -49,10 +50,11 @@ function FeedController($scope, FirebaseService) {
                             console.log('total ::==', total);
                             totolRate += total;
                             _data.gallerys.push(_dataSub);
-
+                            _data.idGal = data.key;
 
                         });
                         _data.total_rate = totolRate; 
+                        _data.idUser = _id;
                         vm.profileList.push(_data);
                         console.log("vm.profileList :: " ,vm.profileList);
                     });
@@ -67,5 +69,68 @@ function FeedController($scope, FirebaseService) {
     }, function (error) {
         console.log("Error: " + error.code);
     });
+    
+    var googleProvider = $rootScope.firebaseUser;
+    var googleId = googleProvider.providerData[0].uid;
+      
+    /*vm.like =  function (userId,idImg) {
+        var refGallery = firebase.database().ref().child('/gallery/'+idImg+'/gallery_rate/');
+        console.log("---------userId--------",userId);
+        console.log("--------idImg---------",idImg);
 
+       
+        refGallery.push({user_id:googleId});
+
+        var count ;
+        var caption;
+        var date_time;
+        var latitude;
+        var location_name;
+        var longitude;
+        var user_id;
+        var _idGal;
+        var gallery_rate;
+        var refGal = firebase.database().ref().child('/gallery');
+       refGal.orderByKey().equalTo(idImg).on("value", function (snapshot) {
+          snapshot.forEach(function (data) {
+                 _idGal = data.key;
+                var _data = snapshot.child(_idGal).val();
+                count = _data.gallary_rate_count + 1 ;
+                  caption  = _data.caption;
+                    date_time  = _data.date_time;
+                    latitude  = _data.latitude;
+                    location_name  = _data.location_name;
+                    longitude  = _data.longitude;
+                    user_id  = _data.user_id;
+                    gallery_rate  = _data.gallery_rate;
+                console.log(">>>>>>>>>>>>>>>>>>>>>",count);
+            });
+
+       });*/
+
+   /*   console.log(">>>>>>>><<<<<<pppppp",idImg);
+      var refUpdateCount = firebase.database().ref().child('/gallery/');
+        var updates = {};
+       updates['/' + idImg] = {
+        caption:caption,
+        date_time:date_time,
+        gallary_rate_count:count,
+        gallery_rate: gallery_rate,
+        latitude:latitude,
+        location_name:location_name,
+        longitude:longitude,
+        user_id:user_id
+
+            };
+      refUpdateCount.update(updates);*/
+
+      
+        
+     
+
+
+
+    }
+
+  
 }
